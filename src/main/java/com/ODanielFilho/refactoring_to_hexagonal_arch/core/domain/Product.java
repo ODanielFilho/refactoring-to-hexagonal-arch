59ff88ab19f5d4c3359da45d67b46d96f1a00245
Daniel Filho <danielfsf2001@gmail.com>
2026-01-22 13:55:21 -0300

    Cria modelo de DOMÍNIO. Agnóstico em relação à Frameworks como Spring. Traz para si a validação de campos delegada a outras camadas no modelo de Arquitetura em Camadas do Spring

---
+++ src/main/java/com/ODanielFilho/refactoring_to_hexagonal_arch/core/domain/Product.java
@@ -0,0 +1,44 @@
+package com.ODanielFilho.refactoring_to_hexagonal_arch.core.domain;
+
+import java.math.BigDecimal;
+
+public class Product {
+    private Long id;
+    private String name;
+    private BigDecimal price;
+
+    public Product(Long id, String name, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void validate() {
+        if (name == null || name.isBlank()) throw new IllegalArgumentException("Invalid name");
+        if (price == null || price.compareTo(BigDecimal.ZERO) <= 0) throw new IllegalArgumentException("Invalid price");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
